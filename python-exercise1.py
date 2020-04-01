88f88ed9f15a76c29545ae424ad26e6aa0d5a9a1
NBESS <nicholasdbess@gmail.com>
2020-04-01 11:39:52 -0400

    Gets user name and greets user

---
+++ python-exercise1.py
@@ -0,0 +1,12 @@
+# Code will find out a user's name and greet that user by name
+
+# Prompt the user for their name.
+# 1. Gather user's name
+user_name = input('What is your name? ')
+
+# 2. Create Greeting
+greeting = f'Hello, {user_name}!'
+
+# 3. Greet user by name
+# Print an output that greets user by name.
+print(greeting)
