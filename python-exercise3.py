f81df5e16a74dba832a719c8397dd6901de248b5
NBESS <nicholasdbess@gmail.com>
2020-04-01 13:46:37 -0400

    adds madlib sentence

---
+++ python-exercise3.py
@@ -0,0 +1,6 @@
+# Prompt a user for missing words in a Madlib sentnce using the input function. Then output sentence displaying user input.
+# Create madlib
+madlib = "___(name)___'s favorite color is ___(color)___"
+# prompt for input
+
+# output madlib sentence with users input
